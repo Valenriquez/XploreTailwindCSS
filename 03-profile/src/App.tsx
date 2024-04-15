85b18a9595c2f6bb8f3513586895a624d44b45ca
Rolando Enriquez Limon <enriquezlimvaleria@outlook.com>
2024-04-15 09:20:23 -0600

    Reapply "First-changes-page"
    This reverts commit 12121d56a89852a6d0bc926523b76871019ec7df.

---
+++ 03-profile/src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+   
+
+  return (
+    <>
+      <div className="container">
+        <h1>Welcome to My Page!</h1>
+        <p>Hi there! I'm Valeria, and this is my personal page.</p>
+        <p>I'm passionate about technology, I love reading and hiking.</p>
+        <p>Feel free to explore and learn more about me!</p>
+      
+
+      <footer className="footer">
+        <div className="social-media">
+          <a href="https://www.pinterest.com/" target="_blank" rel="noopener noreferrer">
+            <img src="public/pint.png" alt="Social Media Icon" />
+          </a>
+          </div>
+        
+      </footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
